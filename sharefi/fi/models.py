b704ec31e5c80a45a59aac9c617409036010cf4a
Kevin Wood <kevin.wood75@gmail.com>
2020-04-26 23:06:39 -0400

    fixed data type

--- sharefi/fi/models.py
+++ sharefi/fi/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Stockinfo(models.Model):
     ticker = models.CharField(max_length=70, blank=False, default='')
-    price = models.IntegerField()
+    price = models.FloatField()
     volume = models.IntegerField()
     stock_date = models.DateTimeField(default=now, blank=True)
 
--- sharefi/sharefi/settings.py
+++ sharefi/sharefi/settings.py
@@ -105,6 +105,8 @@ DATABASES = {
     } 
 }
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
