83e038d6eb0ab577c65d52c6c891badcf09a2826
Kevin Wood <kevin.wood75@gmail.com>
2020-05-10 18:13:04 -0400

    test

--- sharefi/fi/views.py
+++ sharefi/fi/views.py
@@ -76,6 +76,7 @@ def  fi_get_ai_stock_price(request):
         stocks = Stockinfo.objects.all()
         ticker = request.GET.get('ticker', None)
         if ticker is not None:
-            stocks = stocks.filter(ticker__icontains=ticker)
+            stocks = stocks.filter(ticker__icontains=ticker).latest('stock_date')
+        print(stocks)
         fi_serializer = FiSerializer(stocks, many=True)
         return JsonResponse(fi_serializer.data, safe=False)
